b9e6a4ac6fdfe651c87e122eeedd7be3859f862a
yuval3191 <yuval.yam319@gmail.com>
2023-02-21 13:35:10 +0200

    added loginScreen

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
+import LogInScreen from "./src/screens/LogInScreen";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
+    LogIn: LogInScreen,
   },
   {
     initialRouteName: "Home",
---
+++ src/screens/LogInScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button } from "@react-native-material/core";
+
+const LogInScreen = () => (
+  <Button title="Click Me" onPress={() => alert("🎉🎉🎉")}/>
+);
+
+export default LogInScreen;
