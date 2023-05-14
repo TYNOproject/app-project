d210f426f8273aa9aba1e5791277b3d5f32af9e4
Omri Chen <omrichen70@gmail.com>
2023-05-14 09:48:43 +0300

    try google sign in

--- src/screens/HomePageScreen.js
+++ src/screens/HomePageScreen.js
@@ -1,182 +1,188 @@
-import React, {Component, useState, useContext, useEffect} from "react";
-import {StyleSheet, Text, View, FlatList, ActivityIndicator} from "react-native";
-import {ListItem, SearchBar, Card} from "react-native-elements";
-import {useFonts} from "expo-font";
+import React, { Component, useState, useContext, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+import { ListItem, SearchBar, Card } from "react-native-elements";
+import { useFonts } from "expo-font";
 import SelectOption from "../components/SelectOption";
 import CoursesList from "../components/CoursesList";
 import StudentContext from "../contexts/StudentContext";
 import * as constants from "../../constants";
-import {getCoursesByDepartment, searchCourses} from "../api/serviceCalls";
-import {FontAwesome} from "@expo/vector-icons";
-import {useIsFocused} from "@react-navigation/native";
+import { getCoursesByDepartment, searchCourses } from "../api/serviceCalls";
+import { FontAwesome } from "@expo/vector-icons";
+import { useIsFocused } from "@react-navigation/native";
 
+export default function HomePageScreen({ navigation }) {
+  const [search, setSearch] = useState("");
+  const [faculty, setFaculty] = useState(0);
+  const [department, setDepartment] = useState(1);
+  const [year, setYear] = useState(1);
+  const [courses, setCourses] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const isFocused = useIsFocused();
 
-export default function HomePageScreen({navigation}) {
-    const [search, setSearch] = useState("");
-    const [faculty, setFaculty] = useState(0);
-    const [department, setDepartment] = useState(1);
-    const [year, setYear] = useState(1);
-    const [courses, setCourses] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const isFocused = useIsFocused();
+  const { items, getVal, addToStudent } = useContext(StudentContext);
+  const details = getVal(items, "studentDetails");
+  console.log("details: " + details);
+  const name = getVal(items, "studentDetails").name;
+  useEffect(() => {
+    getCoursesByDepartment(getVal(items, "studentDetails").department.id)
+      .then((response) =>
+        response !== undefined ? setCourses(response.data) : setCourses([])
+      )
+      .catch((error) => console.log(error))
+      .finally(() => setIsLoading(false));
+  }, [isFocused]);
 
-    const {items, getVal, addToStudent} = useContext(StudentContext);
-    const name = getVal(items, "studentDetails").name;
-    useEffect(() => {
-        getCoursesByDepartment(getVal(items, "studentDetails").department.id)
-            .then((response) =>
-                response !== undefined ? setCourses(response.data) : setCourses([])
-            )
-            .catch((error) => console.log(error))
-            .finally(() => setIsLoading(false));
-    }, [isFocused]);
+  let [fontsLoaded] = useFonts({
+    "Heebo-Bold": require("../../assets/fonts/Heebo-Bold.ttf"),
+    "Heebo-Regular": require("../../assets/fonts/Heebo-Regular.ttf"),
+  });
 
-    let [fontsLoaded] = useFonts({
-        "Heebo-Bold": require("../../assets/fonts/Heebo-Bold.ttf"),
-        "Heebo-Regular": require("../../assets/fonts/Heebo-Regular.ttf"),
-    });
-
-    const handleSearch = (search) => {
-        setIsLoading(true)
-        let searchDetails = {
-            courseName: search,
-            departmentId: department,
-            year: year,
-        };
-
-        searchCourses(searchDetails)
-            .then((response) => {
-                response !== undefined ? setCourses(response.data) : setCourses([]);
-            })
-            .catch((error) => console.log(error))
-            .finally(() => setIsLoading(false));
+  const handleSearch = (search) => {
+    setIsLoading(true);
+    let searchDetails = {
+      courseName: search,
+      departmentId: department,
+      year: year,
     };
 
-    if (!fontsLoaded)
-        return (
-            <View>
-                <ActivityIndicator size="large" color="#0000ff" />
-            </View>
-        );
+    searchCourses(searchDetails)
+      .then((response) => {
+        response !== undefined ? setCourses(response.data) : setCourses([]);
+      })
+      .catch((error) => console.log(error))
+      .finally(() => setIsLoading(false));
+  };
 
+  if (!fontsLoaded)
     return (
-        <View style={styles.container}>
-
-            <View style={styles.topPart}>
-                <Text style={styles.header}>
-                    היי {name}, {"\n"}
-                    מה נלמד הפעם? {"\n"}
-                </Text>
-            </View>
-            <View style={styles.searchBar}>
-                <SearchBar
-                    placeholder="חיפוש לפי קורס..."
-                    containerStyle={{backgroundColor: "transparent"}}
-                    lightTheme
-                    round
-                    onChangeText={setSearch}
-                    value={search}
-                    autoCorrect={false}
-                    searchIcon={
-                        <FontAwesome
-                            name="search"
-                            size={24}
-                            color="#7521f3"
-                            onPress={() => {
-                                handleSearch(search)
-                            }}
-                        />
-                    }
-                />
-            </View>
-            <View style={styles.dropdown}>
-                <SelectOption
-                    options={constants.departments.map(
-                        (department) => department.department_name
-                    )}
-                    defaultText="מחלקה"
-                    buttonStyle={styles.dropdownButtonStyle}
-                    onSelectOption={(option) => setDepartment(option.id)}
-                />
-                <SelectOption
-                    options={constants.years}
-                    defaultText="שנה"
-                    buttonStyle={styles.dropdownButtonStyle}
-                    onSelectOption={(option) => {
-                        setYear(option);
-                    }}
-                />
-            </View>
-            <View style={styles.spacer}/>
-            <View style={styles.bottomHalf}>
-                {isLoading ? (<ActivityIndicator size="large" color="#0000ff" />) : (
-
-                        <CoursesList
-                            courses={courses}
-                            navigation={navigation}
-                            changeColor = {false}
-                            callback={() => navigation.navigate("CoursePage")}
-                        />
-                    )}
-            </View>
-
-        </View>
+      <View>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
     );
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topPart}>
+        <Text style={styles.header}>
+          היי {name}, {"\n"}
+          מה נלמד הפעם? {"\n"}
+        </Text>
+      </View>
+      <View style={styles.searchBar}>
+        <SearchBar
+          placeholder="חיפוש לפי קורס..."
+          containerStyle={{ backgroundColor: "transparent" }}
+          lightTheme
+          round
+          onChangeText={setSearch}
+          value={search}
+          autoCorrect={false}
+          searchIcon={
+            <FontAwesome
+              name="search"
+              size={24}
+              color="#7521f3"
+              onPress={() => {
+                handleSearch(search);
+              }}
+            />
+          }
+        />
+      </View>
+      <View style={styles.dropdown}>
+        <SelectOption
+          options={constants.departments.map(
+            (department) => department.department_name
+          )}
+          defaultText="מחלקה"
+          buttonStyle={styles.dropdownButtonStyle}
+          onSelectOption={(option) => setDepartment(option.id)}
+        />
+        <SelectOption
+          options={constants.years}
+          defaultText="שנה"
+          buttonStyle={styles.dropdownButtonStyle}
+          onSelectOption={(option) => {
+            setYear(option);
+          }}
+        />
+      </View>
+      <View style={styles.spacer} />
+      <View style={styles.bottomHalf}>
+        {isLoading ? (
+          <ActivityIndicator size="large" color="#0000ff" />
+        ) : (
+          <CoursesList
+            courses={courses}
+            navigation={navigation}
+            changeColor={false}
+            callback={() => navigation.navigate("CoursePage")}
+          />
+        )}
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        top: 20,
-        alignItems: "center",
-        flexDirection: "column",
-        flex: 1,
-    },
-    searchBar: {
-        position: "absolute",
-        direction: "rtl",
-        top: 150,
-        left: 60,
-        height: 60,
-        width: 300,
-    },
-    header: {
-        fontFamily: "Heebo-Bold",
-        fontWeight: "bold",
-        fontSize: 30,
-        shadowOpacity: 0.2,
-        shadowRadius: 1,
-        shadowOffset: {height: 1, width: 0},
-        top: -20,
-        textAlign: "center",
-    },
-    topPart: {
-        position: "absolute",
-        left: "10%",
-        top: 0,
-        width: 320,
-        direction: "rtl",
-        paddingTop: 50,
-    },
-    dropdown: {
-        flexDirection: "row",
-        top: 250,
-        justifyContent: "space-between",
-        alignItems: "center",
-    },
-    dropdownButtonStyle: {
-        flexDirection: "row",
-        justifyContent: "center",
-        width: 110,
-        top: -30,
-    },
+  container: {
+    top: 20,
+    alignItems: "center",
+    flexDirection: "column",
+    flex: 1,
+  },
+  searchBar: {
+    position: "absolute",
+    direction: "rtl",
+    top: 150,
+    left: 60,
+    height: 60,
+    width: 300,
+  },
+  header: {
+    fontFamily: "Heebo-Bold",
+    fontWeight: "bold",
+    fontSize: 30,
+    shadowOpacity: 0.2,
+    shadowRadius: 1,
+    shadowOffset: { height: 1, width: 0 },
+    top: -20,
+    textAlign: "center",
+  },
+  topPart: {
+    position: "absolute",
+    left: "10%",
+    top: 0,
+    width: 320,
+    direction: "rtl",
+    paddingTop: 50,
+  },
+  dropdown: {
+    flexDirection: "row",
+    top: 250,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  dropdownButtonStyle: {
+    flexDirection: "row",
+    justifyContent: "center",
+    width: 110,
+    top: -30,
+  },
 
-    spacer: {
-        flex: 0.7,
-    },
-    bottomHalf: {
-        alignSelf: "flex-end",
-        width: "100%",
-        flex: 2,
-        top: 20,
-    },
+  spacer: {
+    flex: 0.7,
+  },
+  bottomHalf: {
+    alignSelf: "flex-end",
+    width: "100%",
+    flex: 2,
+    top: 20,
+  },
 });
--- src/screens/LogInScreen.js
+++ src/screens/LogInScreen.js
@@ -1,175 +1,254 @@
-import React, {useContext, useState} from "react";
+import React, { useContext, useState, useEffect } from "react";
 import {
-    View,
-    Text,
-    TextInput,
-    StyleSheet,
-    Image,
-    ScrollView,
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Image,
+  ScrollView,
 } from "react-native";
-import {Ionicons} from "@expo/vector-icons";
-import {Button} from "@react-native-material/core";
-import {AntDesign} from "@expo/vector-icons";
-import {useFonts} from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Button } from "@react-native-material/core";
+import { AntDesign } from "@expo/vector-icons";
+import { useFonts } from "expo-font";
 import StudentContext from "../contexts/StudentContext.js";
-import {signIn} from "../api/serviceCalls.js";
-import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view";
+import { signIn, signInWithGoogle } from "../api/serviceCalls.js";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
 
+WebBrowser.maybeCompleteAuthSession();
 
-export default function LoginScreen({navigation}) {
-    const {addToStudent, items, getVal, clearItems} = useContext(StudentContext);
-    const [email, setEmail] = React.useState("");
-    const [password, setPassword] = React.useState("");
-    let [fontsLoaded] = useFonts({
-        "Heebo-Bold": require("../../assets/fonts/Heebo-Bold.ttf"),
-        "Heebo-Regular": require("../../assets/fonts/Heebo-Regular.ttf"),
-    });
-    if (!fontsLoaded)
-        return (
-            <View>
-                <Text>loading</Text>
-            </View>
-        );
-    const handleLogin = async () => {
-        //check if the user exists in the database, if so -> nvaigate to home page
-        //else, pop up that says that he's not registered
-        let signInResponse = await signIn({email, password});
-        if (signInResponse.status !== 200) {
-            alert("אירעה שגיאה, אנא נסה שנית");
-            return;
+export default function LoginScreen({ navigation }) {
+  const { addToStudent, items, getVal, clearItems } =
+    useContext(StudentContext);
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const [token, setToken] = useState("");
+  const [userInfo, setUserInfo] = useState(null);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId:
+      "689467618406-8toie32jg1ef47frm598uognap6hgmfc.apps.googleusercontent.com",
+    iosClientId:
+      "689467618406-ke3kpj2krhu3sl9g59l240t8o8n230vp.apps.googleusercontent.com",
+    expoClientId:
+      "689467618406-scpe9ik85l7uom2kelebldu2oh4uhpa2.apps.googleusercontent.com",
+  });
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      setToken(response.authentication.accessToken);
+      getUserInfo()
+        .then((res) => {
+          addToStudent("studentDetails", res.data);
+          navigation.navigate("HomePage");
+        })
+        .catch((err) => {});
+    }
+  }, [response, token]);
+
+  const getUserInfo = async () => {
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: { Authorization: `Bearer ${token}` },
         }
-        if (signInResponse.status === 200) {
-            addToStudent("studentDetails", signInResponse.data);
-            navigation.navigate("HomePage");
-        }
-    };
+      );
 
+      const user = await response.json();
+      console.log(user);
+      setUserInfo(user);
+    } catch (error) {
+      // Add your own error handler here
+    }
+  };
+
+  let [fontsLoaded] = useFonts({
+    "Heebo-Bold": require("../../assets/fonts/Heebo-Bold.ttf"),
+    "Heebo-Regular": require("../../assets/fonts/Heebo-Regular.ttf"),
+  });
+  if (!fontsLoaded)
     return (
-        <KeyboardAwareScrollView contentContainerStyle={styles.container} style={{flex : 1}}>
-                <Image
-                    source={require("../../assets/images/logo.png")}
-                    style={{
-                        width: "50%",
-                    }}
-                    resizeMode="contain"
-                />
-                <Text style={styles.header}>
-                    {"\n"}
-                    היי,{"\n"}
-                    טוב לראות אותך :)
-                </Text>
-                <View style={styles.inputFields}>
-                    <TextInput
-                        style={styles.inputField1}
-                        placeholder="כתובת מייל"
-                        onChangeText={setEmail}
-                        value={email}
-                    />
-                    <TextInput
-                        style={styles.inputField2}
-                        placeholder="סיסמא"
-                        onChangeText={setPassword}
-                        value={password}
-                        secureTextEntry
-                    />
-                </View>
-                <View style={styles.buttonContainer}>
-                    <Button
-                        title="התחבר"
-                        titleStyle={{
-                            fontSize: 18,
-                            textAlign: "center",
-                            fontFamily: "Heebo-Bold",
-                        }} // Add this line to center the title
-                        leading={() => <AntDesign name="left" size={24} color="white"/>}
-                        onPress={() => {
-                            clearItems();
-                            signIn({email, password}).then((res) => {
-                                addToStudent("studentDetails", res.data);
-                                navigation.navigate("HomePage");
-                            }).catch((err) => alert("אירעה שגיאה, אנא נסה שנית"))
-                        }}
-                    />
-
-                    <Button
-                        title="לא רשום? לחץ כאן"
-                        titleStyle={{
-                            fontSize: 18,
-                            textAlign: "center",
-                            fontFamily: "Heebo-Bold",
-                        }} // Add this line to center the title
-                        leading={() => <AntDesign name="left" size={24} color="white"/>}
-                        onPress={() => {
-                            clearItems();
-                            navigation.navigate("Register")
-                        }}
-                    />
-
-                </View>
-        </KeyboardAwareScrollView>
+      <View>
+        <Text>loading</Text>
+      </View>
     );
+
+  const handleGoogleLogin = async () => {
+    try {
+      let signInResponse = await signIn({ email: userInfo.email }); // use token as password
+      if (signInResponse.status === 200) {
+        addToStudent("studentDetails", signInResponse.data);
+        navigation.navigate("HomePage");
+      } else {
+        // If sign-in fails, navigate to RegisterScreen
+        addToStudent("studentDetails", {
+          name: userInfo.given_name + " " + userInfo.family_name,
+          email,
+        });
+        navigation.navigate("Register");
+      }
+    } catch (error) {
+      alert("אירעה שגיאה, אנא נסה שנית");
+    }
+  };
+
+  return (
+    <KeyboardAwareScrollView
+      contentContainerStyle={styles.container}
+      style={{ flex: 1 }}
+    >
+      <Image
+        source={require("../../assets/images/logo.png")}
+        style={{
+          width: "50%",
+        }}
+        resizeMode="contain"
+      />
+      <Text style={styles.header}>
+        {"\n"}
+        היי,{"\n"}
+        טוב לראות אותך :)
+      </Text>
+      {userInfo === null ? (
+        <>
+          <View style={styles.inputFields}>
+            <TextInput
+              style={styles.inputField1}
+              placeholder="כתובת מייל"
+              onChangeText={setEmail}
+              value={email}
+            />
+            <TextInput
+              style={styles.inputField2}
+              placeholder="סיסמא"
+              onChangeText={setPassword}
+              value={password}
+              secureTextEntry
+            />
+          </View>
+          <View style={styles.buttonContainer}>
+            <Button
+              title="התחבר"
+              titleStyle={{
+                fontSize: 18,
+                textAlign: "center",
+                fontFamily: "Heebo-Bold",
+              }} // Add this line to center the title
+              leading={() => <AntDesign name="left" size={24} color="white" />}
+              onPress={() => {
+                clearItems();
+                signIn({ email, password })
+                  .then((res) => {
+                    addToStudent("studentDetails", res.data);
+                    navigation.navigate("HomePage");
+                  })
+                  .catch((err) => alert("אירעה שגיאה, אנא נסה שנית"));
+              }}
+            />
+
+            <Button
+              title="לא רשום? לחץ כאן"
+              titleStyle={{
+                fontSize: 18,
+                textAlign: "center",
+                fontFamily: "Heebo-Bold",
+              }} // Add this line to center the title
+              leading={() => <AntDesign name="left" size={24} color="white" />}
+              onPress={() => {
+                clearItems();
+                navigation.navigate("Register");
+              }}
+            />
+          </View>
+          <Button
+            title="Sign in with Google"
+            disabled={!request}
+            onPress={() => {
+              promptAsync();
+            }}
+          />
+        </>
+      ) : (
+        navigation.navigate("HomePage")
+      )}
+    </KeyboardAwareScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-    keyboardAvoidingView: {
-        padding: "10%",
-        height: "50%"
-    },
-    container: {
-        flex: 1,
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "space-between",
-        marginTop: "15%",
-    },
-    inputFields: {
-        width: "80%",
-        flex: 1,
-        justifyContent: "space-evenly",
-        marginTop: "10%",
-    },
-    header: {
-        fontSize: 30, // use responsive font size
-        fontFamily: "Heebo-Bold",
-        textAlign: "center",
-        textShadowColor: "#A1B2C3",
-        textShadowOffset: {width: 1},
-        textShadowRadius: 7,
-    },
-    inputField1: {
-        backgroundColor: "#fff",
-        paddingVertical: 10,
-        paddingHorizontal: 15,
-        borderColor: "#7521f3",
-        borderWidth: 2,
-        borderRadius: 15,
-        fontSize: 16,
-        direction: "rtl",
-        textAlign: "right",
-        fontFamily: "Heebo-Regular",
-    },
+  keyboardAvoidingView: {
+    padding: "10%",
+    height: "50%",
+  },
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginTop: "15%",
+  },
+  inputFields: {
+    width: "80%",
+    flex: 1,
+    justifyContent: "space-evenly",
+    marginTop: "10%",
+  },
+  header: {
+    fontSize: 30, // use responsive font size
+    fontFamily: "Heebo-Bold",
+    textAlign: "center",
+    textShadowColor: "#A1B2C3",
+    textShadowOffset: { width: 1 },
+    textShadowRadius: 7,
+  },
+  inputField1: {
+    backgroundColor: "#fff",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderColor: "#7521f3",
+    borderWidth: 2,
+    borderRadius: 15,
+    fontSize: 16,
+    direction: "rtl",
+    textAlign: "right",
+    fontFamily: "Heebo-Regular",
+  },
 
-    inputField2: {
-        backgroundColor: "#fff",
-        paddingVertical: 10,
-        paddingHorizontal: 15,
-        borderColor: "#7521f3",
-        borderWidth: 2,
-        borderRadius: 15,
-        fontSize: 16,
-        direction: "rtl",
-        textAlign: "right",
-        fontFamily: "Heebo-Regular",
-    },
-    buttonContainer: {
-        flex: 1,
-        width: "80%",
-        alignItems: "center",
-        justifyContent: "space-evenly",
-        marginBottom: "30%",
-    },
-    buttonText: {
-        fontSize: 18,
-        textAlign: "center",
-    },
+  inputField2: {
+    backgroundColor: "#fff",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderColor: "#7521f3",
+    borderWidth: 2,
+    borderRadius: 15,
+    fontSize: 16,
+    direction: "rtl",
+    textAlign: "right",
+    fontFamily: "Heebo-Regular",
+  },
+  buttonContainer: {
+    flex: 1,
+    width: "80%",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    marginBottom: "30%",
+  },
+  buttonText: {
+    fontSize: 18,
+    textAlign: "center",
+  },
+  container1: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text1: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
 });
